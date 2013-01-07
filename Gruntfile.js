d537ce467aa090780c33dc51bd1fa783202ec7a2
suisho <suisho000000@gmail.com>
2013-01-07 16:03:19 +0900

    create test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,68 @@
 'use strict';
+var fs = require('fs-extra');
+var touch = require('touch');
+var assert = require('assert');
+var path = require('path');
+function _setup(dir){
+  fs.mkdirsSync(dir);
+  fs.mkdirsSync(dir+'/empty');
+  fs.mkdirsSync(dir+'/not_empty');
+  touch.sync(dir+'/not_empty/foo.txt');
+  fs.mkdirsSync(dir+'/tree/tree/empty');
+  fs.mkdirsSync(dir+'/tree/tree/not_empty');
+  touch.sync(dir+'/tree/tree/not_empty/foo.txt');
+}
+function _assert(dir, name){
+  name = name || ".gitkeep";
+  assert.ok(fs.existsSync(path.join(dir, '/empty/'+name)))
+  assert.ok(!fs.existsSync(path.join(dir,'/not_empty/'+name)))
+  assert.ok(fs.existsSync(path.join(dir, '/tree/tree/empty/'+name)))
+  assert.ok(!fs.existsSync(path.join(dir,'/tree/tree/not_empty/'+name)))
+
+}
 module.exports = function(grunt){
   grunt.initConfig({
-    jshint : {
-      all : [
-        'Gruntfile.js',
-        'tasks/*.js',
-        'test/*.js'
-      ],
-      jshintrc: '.jshintrc'
-    },
     watch : {
       all : {
         files : ['<%= jshint.all %>'],
         tasks : ['default'],
         options: {interrupt: true}
       }
+    },
+    clean: {
+      test: ['tmp/short', 'tmp/long', 'tmp/opt']
+    },
+    gitkeep : {
+      short : ['tmp/short'],
+      long : ['tmp/long'],
+      opt: {
+        src : ['tmp/opt'],
+        options : {
+          keepFileName : 'keepme'
+        }
+      }
+    },
+    options : {
+      'stack' : true
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('setup',function(){
+    _setup('tmp/short')
+    _setup('tmp/long')
+    _setup('tmp/opt')
+  });
+  grunt.registerTask('assert', function(){
+    try{
+      _assert('tmp/short')
+      _assert('tmp/long')
+      _assert('tmp/opt', 'keepme')
+    }catch(e){
+      grunt.fatal(e)
+    }
+  })
+  grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default',['jshint']);
+  grunt.registerTask('test',['clean', 'setup', 'gitkeep', 'assert']);
+  grunt.registerTask('default',['test']);
 };
